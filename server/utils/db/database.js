768eb9b9bb324c4ca4ae00eff232d66e2228ac9c
syedsumair14 <syedsumair14@gmail.com>
2020-06-19 16:32:01 +0530

    node basic setup

--- server/utils/db/database.js
+++ server/utils/db/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2')
 
 const connection = mysql.createPool({
-    host: '',
-    user: '',
-    password: '',
-    database: ''
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DB_NAME
 })
 
 module.exports = connection.promise()
